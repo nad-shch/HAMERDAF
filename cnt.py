4da9368e03095ea89ee2d28c562c611f6ee55ed8
nad-shch <71437024+nad-shch@users.noreply.github.com>
2021-04-24 09:02:15 +0300

    Update cnt.py

--- cnt.py
+++ cnt.py
@@ -38,28 +38,50 @@ send(msg)
 if msg == '*':
     send(DISCONNECT_MESSAGE)
 
-def round1():
-    re = client.recv(1024).decode(FORMAT)
-    print(re)
+
+
+def round1(message, submit):
+    question = client.recv(1024).decode(FORMAT)
     time_left = client.recv(1024).decode(FORMAT)
-    print(f"you have {time_left} seconds")
-    answer=input("type your answer: ")
+    question = DavidFont.render(question, 1, (0, 0, 0))
+    time_left = DavidFont.render(time_left, 1, (0, 0, 0))
+    answer = DavidFont.render(message, 1, (0, 0, 0))
+    WIN.blit(time_left, (10, 10))
+    WIN.blit(question, (10, 100))
+    WIN.blit(f"your answer: {answer}")
+    pygame.display.update()
     if answer == "*":
         send(DISCONNECT_MESSAGE)
-    client.send(answer.encode())
-    re = client.recv(2048).decode(FORMAT)
-    print(re)
+    if submit == True:
+        client.send(answer.encode())
+        re = client.recv(2048).decode(FORMAT)
+        re = DavidFont.render(re, 1, (0, 0, 0))
+
 
 
 
 def main():
     run = True
+    round_num = 1
+    message = ''
     while run:
+        submit = False
         for event in pygame.event.get():
             if event.type == DISCONNECT_MESSAGE:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    message += 'a'
+                elif event.key == pygame.K_b:
+                    message += 'b'
+                elif event.key == pygame.K_c:
+                    message += 'c'
+                elif event.key == pygame.K_d:
+                    message += 'd'
+                elif event.key == pygame.K_e:
+                    message += 'e'
     WIN.blit(BACKGROND, (0, 0))
-    round1()
+    round1(message, submit)
     pygame.display.update()
 
 
@@ -69,4 +91,4 @@ if __name__ =='__main__' and msg == "start":
 
 
 
-    
+print("Hello World")
